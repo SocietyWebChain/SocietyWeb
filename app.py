6f9600b83efb73245c3bbe1aa0a17feddc20814c
BurakEArık <burakar1912@gmail.com>
2025-07-22 22:13:17 +0300

    app.py güncelleme

--- app.py
+++ app.py
@@ -199,6 +199,6 @@ def change_password():
     else:
         return jsonify({"error": "Update failed"}), 400
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT",5000))
-    app.run(host='0.0.0.0', port=port)
+#if __name__ == "__main__":
+#    port = int(os.environ.get("PORT",5000))
+#    app.run(host='0.0.0.0', port=port)
