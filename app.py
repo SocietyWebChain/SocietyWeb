79fd0af43672325bee31aa5ba21399ed1eda9e6e
pimogbu <pimogbu@gmail.com>
2025-07-28 13:21:27 +0300

    degub mode false

--- app.py
+++ app.py
@@ -310,5 +310,5 @@ def resetting_password():
                                  refresh_token=refresh_token)
         
                                  
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+#    app.run(debug=True)
