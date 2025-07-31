94957cb0966317dc2514c06c04d8ed769a2bd493
BurakEArık <burakar1912@gmail.com>
2025-07-31 16:56:43 +0300

    8

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify, abort
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, send_from_directory, abort
 from supabase import create_client, Client
 from dotenv import load_dotenv
 import re
@@ -39,7 +39,7 @@ def gizlilik():
 
 @app.route('/ads.txt')
 def ads_txt():
-    return render_template('static', 'ads.txt')
+    return send_from_directory('static', 'ads.txt')
 
 @cache.memoize(timeout=60)
 def is_user_banned(user_id):
